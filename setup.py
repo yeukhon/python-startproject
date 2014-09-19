9d56a9dda85e1cfb8486a9e34482eaf497e4254b
Yeuk Hon Wong <yeukhon@acm.org>
2014-09-19 18:08:12 -0400

    Initialize repo skeleton

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="python-startproject",
+    version="0.1",
+    description="Provides a command to create Python package skeleton",
+    author="Yeuk Hon Wong",
+    author_email="yeukhon@acm.org",
+    packages=find_packages(),
+    install_requires=[]
+)
