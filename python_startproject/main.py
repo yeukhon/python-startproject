b78950b9e9f5493e2f9e81cf65fb49d6c70a3316
Yeuk Hon Wong <yeukhon@acm.org>
2014-09-20 18:53:14 -0400

    Working on #1 added template_setuppy and broke the first test.

--- python_startproject/main.py
+++ python_startproject/main.py
@@ -1,13 +1,61 @@
+import collections
 import os
+import jinja2
+
+Entry = collections.namedtuple("Entry", ["name", "default"])
+
+def set_project_name(project_name):
+    return project_name
+
+def set_version(version=None):
+    if version:
+        return version
+    return "0.1"
+
+def set_description(description=None):
+    return description or ""
+
+def set_author(author=None):
+    return author or ""
+
+def set_author_email(author_email=None):
+    return author_email or ""
+
+def set_packages():
+    return "find_packages()"
+
+def set_install_requires(requires):
+    if requires:
+        return ",".join(requires)
+
+SETUPPY_TEMPLATE = """\
+from setuptools import setup, find_packages
+
+setup(
+    name="{{ name }}",
+    version="{{ version }}",
+    description="{{ description }}",
+    author="{{ author }}",
+    author_email=" {{ email }}",
+    packages={{ packages }},
+    install_requires=[{{ install_requires }}]
+)
+"""
 
 DEFAULTS = [
-    "name",
-    "version",
-    "description",
-    "author",
-    "packages",
-    "install_requires"
+    Entry(name="name", default=set_project_name),
+    Entry(name="version", default=set_version),
+    Entry(name="description", default=set_description),
+    Entry(name="author", default=set_author),
+    Entry(name="packages", default=set_packages),
+    Entry(name="install_requires", default=set_install_requires)
 ]
+
+def template_setuppy(configs):
+    template = jinja2.Template(SETUPPY_TEMPLATE)
+    output = template.render(configs)
+    print output
+    return output
 
 def create_project(project_name):
     curr_dir = os.getcwd()
--- tests/functional/test_main.py
+++ tests/functional/test_main.py
@@ -8,11 +8,24 @@ except ImportError:
 
 from python_startproject import main
 
-class TestMainAsFunctions(unittest.TestCase):
+class BaseTestCase(unittest.TestCase):
+    __test__ = False
+
     def setUp(self):
         # Creates a temporary directory as a base directory
         self.home_dir = tempfile.mkdtemp()
         self.project_name = "sample"
+
+        # Default checklist. These are hardcoded but we can
+        # the difference against the code in our tests.
+        self.default_version = "0.1"
+        self.default_description = ""
+        self.default_author = ""
+        self.default_packages = "find_packages()"
+        self.default_install_requires = ""
+
+    def tearDown(self):
+        os.rmdir(self.home_dir)
 
     def read_setuppy(self, path):
         setuppy_path = self._full_path(path, "setup.py")
@@ -26,13 +39,53 @@ class TestMainAsFunctions(unittest.TestCase):
         self.assertTrue(
             os.path.exists(full_path))
 
-    def assert_setuppy_is_default(self, path):
-        text = self.read_setuppy(path)
-        for default in main.DEFAULTS:
-            self.assert_setuppy_has(text, default + '=""')
+    def assert_name(self, text, pattern):
+        self.assertTrue(
+            'name="' + pattern + '"' in text
+        )
 
-    def assert_setuppy_has(self, text, pattern):
-        self.assertTrue(pattern in text)
+    def assert_version(self, text, pattern):
+        self.assertTrue(
+            'version="' + pattern + '"' in text
+        )
+
+    def assert_description(self, text, pattern):
+        self.assertTrue(
+            'description="' + pattern + '"' in text
+        )
+
+    def assert_author(self, text, pattern):
+        self.assertTrue(
+            'author="' + pattern + '"' in text
+        )
+
+    def assert_packages(self, text, pattern):
+        self.assertTrue(
+            'packages=' + pattern in text
+        )
+
+    def assert_install_requires(self, text, pattern):
+        self.assertTrue(
+            'install_requires=[' + pattern + ']' in text
+        )
+
+    def assert_setuppy_file_used_default(self, path):
+        text = self.read_setuppy(path)
+        self.assert_setuppy_used_default(text)
+
+    def assert_setuppy_used_default(self, text):
+        self.assert_name(text, self.project_name)
+        self.assert_version(text, self.default_version)
+        self.assert_description(text, self.default_description)
+        self.assert_author(text, self.default_author)
+        self.assert_packages(text, self.default_packages)
+        self.assert_install_requires(text,
+            self.default_install_requires)
+
+class TestMainAsFunction(BaseTestCase):
+
+    def setUp(self):
+        super(TestMainAsFunction, self).setUp()
 
     @patch("os.getcwd") 
     def test_create_project_with_default_options(self, mk_cwd):
@@ -45,4 +98,22 @@ class TestMainAsFunctions(unittest.TestCase):
         project_path = self._full_path(self.home_dir,
             self.project_name)
         self.assert_dir_created(project_path)
-        self.assert_setuppy_is_default(project_path)
+        self.assert_setuppy_file_is_default(project_path)
+
+
+class TestTemplateSetuppy(BaseTestCase):
+
+    def setUp(self):
+        super(TestTemplateSetuppy, self).setUp()
+        self.configs = dict(
+            name=self.project_name,
+            version=self.default_version,
+            description=self.default_description,
+            author=self.default_author,
+            packages=self.default_packages,
+            install_requires=self.default_install_requires
+        )
+
+    def test_setuppy_with_defaults(self):
+        setuppy = main.template_setuppy(self.configs)
+        self.assert_setuppy_used_default(setuppy)
